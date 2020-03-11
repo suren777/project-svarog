06e20d90b51c03cbe8a3ed094c892e115362e2b0
si777 <suren.islyaev@gmail.com>
2020-03-11 23:10:55 +0000

    Adding procfile

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ from layout.index_layout import generate_main_layout
 from app import app
 
 app.layout = generate_main_layout()
-
+server = app.server
 if __name__ == "__main__":
     app.run_server(debug=True, host="192.168.86.117")
 
