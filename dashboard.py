8e1da551b6c2e38430731678b108acc77a974cd0
si777 <suren.islyaev@gmail.com>
2020-03-12 12:36:49 +0000

    Removing host

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,7 @@ from app import app
 
 app.layout = generate_main_layout()
 server = app.server
+
 if __name__ == "__main__":
-    app.run_server(debug=True, host="192.168.86.117")
+    app.run_server(debug=True)
 
